71c9ccd6b3f848c8f50809624390181126b7877e
Masee Hussain <hussainmasee@gmail.com>
2018-07-04 12:03:23 +0100

    higher order component
    created a hoc folder and an Aux.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 
+import Layout from "./components/Layout/Layout";
+
 class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>Building a Burger</h1>
-        <h2>Best Burger in Town</h2>
+        <Layout>
+          <h1>Building a Burger</h1>
+          <h2>Best Burger in Town</h2>
+        </Layout>
       </div>
     );
   }
