b36690e22becc7c48786bd85b808692a20445b5f
Masee Hussain <hussainmasee@gmail.com>
2018-07-04 11:43:25 +0100

    restructured files
    added app.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Building a Burger</h1>
+        <h2>Best Burger in Town</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-import "./styles.css";
-
-function App() {
-  return (
-    <div className="App">
-      <h1>Building a Burger</h1>
-      <h2>Best Burger in Town</h2>
-    </div>
-  );
-}
+import "./index.css";
+import App from "./App";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
