f358701c7c00d5a600d7600d0280a4d336728ed0
Masee Hussain <hussainmasee@gmail.com>
2018-07-04 13:09:11 +0100

    refactored ingredient into class component

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,39 +1,42 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 import "./BurgerIngredient.css";
 
-const burgerIngredient = props => {
-  let ingredient = null;
+class BurgerIngredient extends React.Component {
+  render() {
+    let ingredient = null;
 
-  switch (props.type) {
-    case "bread-bottom":
-      ingredient = <div className="BreadBottom" />;
-      break;
-    case "bread-top":
-      ingredient = (
-        <div className="BreadTop">
-          <div className="Seeds1" />
-          <div className="Seeds2" />
-        </div>
-      );
-      break;
-    case "meat":
-      ingredient = <div className="Meat" />;
-      break;
-    case "cheese":
-      ingredient = <div className="Cheese" />;
-      break;
-    case "salad":
-      ingredient = <div className="Salad" />;
-      break;
-    case "bacon":
-      ingredient = <div className="Bacon" />;
-      break;
-    default:
-      ingredient = null;
+    switch (props.type) {
+      case "bread-bottom":
+        ingredient = <div className="BreadBottom" />;
+        break;
+      case "bread-top":
+        ingredient = (
+          <div className="BreadTop">
+            <div className="Seeds1" />
+            <div className="Seeds2" />
+          </div>
+        );
+        break;
+      case "meat":
+        ingredient = <div className="Meat" />;
+        break;
+      case "cheese":
+        ingredient = <div className="Cheese" />;
+        break;
+      case "salad":
+        ingredient = <div className="Salad" />;
+        break;
+      case "bacon":
+        ingredient = <div className="Bacon" />;
+        break;
+      default:
+        ingredient = null;
+    }
+
+    return ingredient;
   }
+}
 
-  return ingredient;
-};
-
-export default burgerIngredient;
+export default BurgerIngredient;
