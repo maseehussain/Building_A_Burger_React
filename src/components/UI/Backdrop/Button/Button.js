dabee6ad714051f9a85f1687e7be23d16a8802b9
Masee Hussain <hussainmasee@gmail.com>
2018-07-05 12:57:55 +0100

    added the button.js and also implemented the click event for continue and cancel

--- src/components/UI/Backdrop/Button/Button.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const button = props => (
-  <button onClick={props.clicked}>{props.children}</button>
-);
-
-export default button;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -76,6 +76,10 @@ class BurgerBuilder extends React.Component {
     this.setState({ purchasing: false });
   };
 
+  purchaseContinueHandler = () => {
+    alert("You Continue!");
+  };
+
   render() {
     const disabledInfo = {
       ...this.state.ingredients
@@ -90,7 +94,11 @@ class BurgerBuilder extends React.Component {
           show={this.state.purchasing}
           modalClosed={this.purchaseCancelHandler}
         >
-          <OrderSummary ingredients={this.state.ingredients} />
+          <OrderSummary
+            ingredients={this.state.ingredients}
+            purchaseCancelled={this.purchaseCancelHandler}
+            purchaseContinued={this.purchaseContinueHandler}
+          />
         </Modal>
         <Burger ingredients={this.state.ingredients} />
         <BuildControls
