962824462f20ce47b7802eebd0d005de5ce1cc86
Masee Hussain <hussainmasee@gmail.com>
2018-07-05 09:50:05 +0100

    created directories and files
    modal.js and backdrop.js

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -4,10 +4,10 @@ import "./BuildControls.css";
 import BuildControl from "./BuildControl/BuildControl";
 
 const controls = [
-  { label: "Salad", type: "salad" },
   { label: "Bacon", type: "bacon" },
   { label: "Cheese", type: "cheese" },
-  { label: "Meat", type: "meat" }
+  { label: "Meat", type: "meat" },
+  { label: "Salad", type: "salad" }
 ];
 
 const buildControls = props => (
