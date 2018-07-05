eda94efc12972df4d7ebf9da64ebb9a6e3bd5eed
Masee Hussain <hussainmasee@gmail.com>
2018-07-05 11:51:43 +0100

    within Backdrop.js add a onClick

--- src/components/UI/Backdrop/Backdrop.js
+++ src/components/UI/Backdrop/Backdrop.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import "./Backdrop.css";
 
-const backdrop = props => (props.show ? <div className="Backdrop" onClick={} /> : null);
+const backdrop = props =>
+  props.show ? <div className="Backdrop" onClick={props.clicked} /> : null;
 
 export default backdrop;
