04109eb13a7a7823c664d687f0b77019bfedc79c
Masee Hussain <hussainmasee@gmail.com>
2018-07-06 11:21:25 +0100

    Changed the Layout component into a class
    as we need to create a click handler

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -5,12 +5,15 @@ import "./Layout.css";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
 import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
-const layout = props => (
-  <Aux>
-    <Toolbar />
-    <SideDrawer />
-    <main className="Content">{props.children}</main>
-  </Aux>
-);
-
-export default layout;
+class Layout extends React.Component {
+  render() {
+    return (
+      <Aux>
+        <Toolbar />
+        <SideDrawer />
+        <main className="Content">{this.props.children}</main>
+      </Aux>
+    );
+  }
+}
+export default Layout;
