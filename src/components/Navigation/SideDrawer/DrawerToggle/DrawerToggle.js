7529460e1ca0a85e7c70043e44c6e371007f8e00
Masee Hussain <hussainmasee@gmail.com>
2018-07-31 09:25:55 +0100

    hamburger icon now works for small devices

--- src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -1,6 +1,12 @@
 import React from "react";
 import "./DrawerToggle.css";
 
-const drawerToggle = props => <div onClick={props.clicked}>MENU</div>;
+const drawerToggle = props => (
+  <div className="DrawerToggle" onClick={props.clicked}>
+    <div />
+    <div />
+    <div />
+  </div>
+);
 
 export default drawerToggle;
