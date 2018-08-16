9fe3b481f22d2cb5a60fa712cbc817490a51c30e
Masee Hussain <hussainmasee@gmail.com>
2018-08-16 17:06:47 +0100

    corrected message now shows once there is an error

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -29,8 +29,8 @@ const withErrorHandler = (WrappedComponent, Axios) => {
     render() {
       return (
         <Aux>
-          <Modal show={this.state.error} clicked={this.errorConfirmHandler}>
-            {this.state.error ? this.state.message : null}
+          <Modal show={this.state.error} modalClosed={this.errorConfirmHandler}>
+            {this.state.error ? this.state.error.message : null}
           </Modal>
           <WrappedComponent {...this.props} />
         </Aux>
