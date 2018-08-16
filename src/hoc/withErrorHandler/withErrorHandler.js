39e3a3ded751b130d3ab22ef2b6e7de3c6449132
Masee Hussain <hussainmasee@gmail.com>
2018-08-16 16:40:07 +0100

    import files to burgerBuilder and wrap it withErrorHandler

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -1,8 +1,16 @@
 import React from "react";
 
+import Modal from "../../components/UI/Modal/Modal";
+import Aux from "../Aux/Aux";
+
 const withErrorHandler = WrappedComponent => {
   return props => {
-    return <WrappedComponent {...props} />;
+    return (
+      <Aux>
+        <Modal>Something didn't work!</Modal>
+        <WrappedComponent {...props} />
+      </Aux>
+    );
   };
 };
 
