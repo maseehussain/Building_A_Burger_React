c145bc3301263fc4e60d4d9049be46d6ba19b23c
Masee Hussain <hussainmasee@gmail.com>
2018-09-04 15:38:56 +0100

    add an ingredients object to store the order

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -14,6 +14,7 @@ class Checkout extends React.Component {
 
   componentDidMount() {
     const query = new URLSearchParams(this.props.location.search);
+    const ingredients = {};
   }
 
   checkoutCancelledHandler = () => {
