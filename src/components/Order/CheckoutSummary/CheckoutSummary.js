affbbcb2fdf34f8c6fc13683f52c1b34c8c9d295
Masee Hussain <hussainmasee@gmail.com>
2018-09-04 12:26:15 +0100

    tested my checkout display on App.js

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -8,7 +8,7 @@ const checkoutSummary = props => {
   return (
     <div className="CheckoutSummary">
       <h1>We hope it tastes great!</h1>
-      <div style={{ width: "300px", height: "300px", margin: "auto" }}>
+      <div style={{ width: "100%", margin: "auto" }}>
         <Burger ingredients={props.ingredients} />
       </div>
       <Button btnType="Danger" clicked>
