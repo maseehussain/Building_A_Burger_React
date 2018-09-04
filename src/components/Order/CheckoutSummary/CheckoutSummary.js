36e6e68d2a74899f59d25f9c608c271959c8195b
Masee Hussain <hussainmasee@gmail.com>
2018-09-04 11:59:21 +0100

    imported my burger component

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -1,9 +1,14 @@
 import React from "react";
 
+import Burger from "../../Burger/Burger";
+
 const checkoutSummary = props => {
   return (
     <div>
       <h1>We hope it tastes great!</h1>
+      <div style={{ width: "300px", height: "300px", margin: "auto" }}>
+        <Burger />
+      </div>
     </div>
   );
 };
