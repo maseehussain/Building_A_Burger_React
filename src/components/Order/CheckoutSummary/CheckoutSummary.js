6c7345f1842f2d5c8f41008e8683ca9bc01512a4
Masee Hussain <hussainmasee@gmail.com>
2018-09-04 11:54:14 +0100

    created my functional form for checkoutSummary

---
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const checkoutSummary = props => {
+  return (
+    <div>
+      <h1>We hope it tastes great!</h1>
+    </div>
+  );
+};
+
+export default checkoutSummary;
