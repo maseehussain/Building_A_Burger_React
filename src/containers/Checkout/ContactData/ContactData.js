e392e52589f167f33c67e2d85adecc897dae2e12
Masee Hussain <hussainmasee@gmail.com>
2018-09-04 16:22:18 +0100

    added my contact list

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,17 +1,29 @@
 import React from "react";
 
+import Button from "../../../components/UI/Button/Button";
+
 class ContactData extends React.Component {
   state = {
     name: "",
     email: "",
     address: {
       street: "",
-      city: "",
       postCode: ""
     }
   };
   render() {
-    return ();
+    return (
+      <div>
+        <h4>Enter your Contact Data</h4>
+        <form>
+          <input type="text" name="name" placeholder="Your Name" />
+          <input type="email" name="email" placeholder="Your Email" />
+          <input type="text" name="street" placeholder="Street" />
+          <input type="text" name="post code" placeholder="Post Code" />
+          <Button btnType="Success">ORDER</Button>
+        </form>
+      </div>
+    );
   }
 }
 
