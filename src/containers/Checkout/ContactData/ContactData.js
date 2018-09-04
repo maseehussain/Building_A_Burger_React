3590bef578833ebaaabb2e93facb04b56e1a3a20
Masee Hussain <hussainmasee@gmail.com>
2018-09-04 16:07:14 +0100

    created my class component with the state data

---
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class ContactData extends React.Component {
+  state = {
+    name: "",
+    email: "",
+    address: {
+      street: "",
+      city: "",
+      postCode: ""
+    }
+  };
+  render() {
+    return ();
+  }
+}
+
+export default ContactData;
