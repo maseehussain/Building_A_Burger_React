1b3feafde3dc3ba2d2da76e0204d474f0b6fbe20
Masee Hussain <hussainmasee@gmail.com>
2018-09-04 12:09:51 +0100

    added styling

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -1,14 +1,22 @@
 import React from "react";
 
 import Burger from "../../Burger/Burger";
+import Button from "../../UI/Button/Button";
+import "./CheckoutSummary.css";
 
 const checkoutSummary = props => {
   return (
-    <div>
+    <div className="CheckoutSummary">
       <h1>We hope it tastes great!</h1>
       <div style={{ width: "300px", height: "300px", margin: "auto" }}>
         <Burger ingredients={props.ingredients} />
       </div>
+      <Button btnType="Danger" clicked>
+        CANCEL
+      </Button>
+      <Button btnType="Success" clicked>
+        CONTINUE
+      </Button>
     </div>
   );
 };
