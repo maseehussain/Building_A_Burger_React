c8a5a88c2014ac790b6d6a90e2272d2f8c116f34
Masee Hussain <hussainmasee@gmail.com>
2018-09-05 12:24:55 +0100

    converted object into an array with a for loop

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -12,7 +12,10 @@ class Orders extends React.Component {
   componentDidMount() {
     Axios.get("/orders.json")
       .then(res => {
-        console.log(res.data);
+        const fetchedOrders = [];
+        for (let key in res.data) {
+          fetchedOrders.push(res.data[key]);
+        }
         this.setState({ loading: false });
       })
       .catch(err => {
