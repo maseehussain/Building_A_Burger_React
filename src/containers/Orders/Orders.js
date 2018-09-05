45d9236fa59e9cd1ba8bcc13442fbfa0629bb233
Masee Hussain <hussainmasee@gmail.com>
2018-09-05 11:16:12 +0100

    display dummy data from Oder.js

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -3,8 +3,13 @@ import React from "react";
 import Order from "../../components/Order/Order";
 
 class Orders extends React.Component {
-  render () {
-    return ();
+  render() {
+    return (
+      <div>
+        <Order />
+        <Order />
+      </div>
+    );
   }
 }
 
