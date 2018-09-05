8387d5df4bf1b552bae6d97b38a5693e21d1e2c0
Masee Hussain <hussainmasee@gmail.com>
2018-09-05 12:28:18 +0100

    imported withErrorHandler

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import Order from "../../components/Order/Order";
 import Axios from "../../axios-orders";
+import withErrorHandler from "../../hoc/withErrorHandler/withErrorHandler";
 
 class Orders extends React.Component {
   state = {
