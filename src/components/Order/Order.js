3314e0f2a17b5d9ae3d83d0f7a064503d79d44bd
Masee Hussain <hussainmasee@gmail.com>
2018-09-05 15:03:56 +0100

    data displays with some inline styling

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -14,7 +14,16 @@ const order = props => {
 
   const ingredientOutput = ingredients.map(ig => {
     return (
-      <span>
+      <span
+        style={{
+          textTransform: "capitalize",
+          display: "inline-block",
+          margin: "0 8px",
+          border: "1px solid #ccc",
+          padding: "5px"
+        }}
+        key={ig.name}
+      >
         {ig.name} ({ig.amount})
       </span>
     );
