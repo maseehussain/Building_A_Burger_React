0f672bec7250bca336dc229ec36b8483caef3bb1
Masee Hussain <hussainmasee@gmail.com>
2018-09-05 11:07:13 +0100

    made my functional componet with dummy order

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,7 +1,12 @@
 import React from "react";
 
-const order = (props) => (
-  
+const order = props => (
+  <div>
+    <p>Ingredients: Salad (1)</p>
+    <p>
+      Price: <strong>£ 5.50</strong>
+    </p>
+  </div>
 );
 
 export default order;
