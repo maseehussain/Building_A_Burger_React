5e7e583df443e1c6d19253f43bcd58444b5bf7d4
Masee Hussain <hussainmasee@gmail.com>
2018-09-05 12:14:01 +0100

    imported axios

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Order from "../../components/Order/Order";
+import Axios from "../../axios-orders";
 
 class Orders extends React.Component {
   componentDidMount() {}
