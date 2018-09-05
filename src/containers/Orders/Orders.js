02f5592dfb93834632e87e86181d7099568edc3b
Masee Hussain <hussainmasee@gmail.com>
2018-09-05 12:17:07 +0100

    created state with orders and loading set as true

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -4,6 +4,11 @@ import Order from "../../components/Order/Order";
 import Axios from "../../axios-orders";
 
 class Orders extends React.Component {
+  state = {
+    orders: [],
+    loading: true
+  };
+
   componentDidMount() {
     Axios.get("/orders.json").then(res => {});
   }
