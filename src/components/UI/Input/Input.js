6832c73575643750148bf3350ef6e0addcb974d1
Masee Hussain <hussainmasee@gmail.com>
2018-09-06 10:00:31 +0100

    need to create a switch statement for my form output

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -1,7 +1,11 @@
 import React from "react";
 
-const input = (props) => (
-  
-);
+const input = props => {
+  return (
+    <div>
+      <label>{props.label}</label>
+    </div>
+  );
+};
 
-  export default input;
+export default input;
