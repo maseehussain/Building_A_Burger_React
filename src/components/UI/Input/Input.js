835bdc8fd41660641d276f705898a93e90f941d1
Masee Hussain <hussainmasee@gmail.com>
2018-09-06 10:20:12 +0100

    added styling to my input form

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -1,22 +1,24 @@
 import React from "react";
 
+import "./Input.css";
+
 const input = props => {
   let inputElement = null;
 
   switch (props.inputType) {
     case "input":
-      inputElement = <input {...props} />;
+      inputElement = <input className="InputElement" {...props} />;
       break;
     case "textarea":
-      inputElement = <textarea {...props} />;
+      inputElement = <textarea className="InputElement" {...props} />;
       break;
     default:
-      inputElement = <input {...props} />;
+      inputElement = <input className="InputElement" {...props} />;
   }
 
   return (
-    <div>
-      <label>{props.label}</label>
+    <div className="Input">
+      <label className="Label">{props.label}</label>
       {inputElement}
     </div>
   );
