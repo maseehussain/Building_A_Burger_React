caf66cca98c5472de39ff78e40d81e577cdc19ce
Masee Hussain <hussainmasee@gmail.com>
2018-09-07 11:21:02 +0100

    FOrm displays and placeholder is working

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -96,7 +96,7 @@ class ContactData extends React.Component {
             key={formElement.id}
             elementType={formElement.config.elementType}
             elementConfig={formElement.config.elementConfig}
-            value={formElement.config.elementConfig}
+            value={formElement.config.value}
           />
         ))}
         <Button btnType="Success" clicked={this.orderHandler}>
