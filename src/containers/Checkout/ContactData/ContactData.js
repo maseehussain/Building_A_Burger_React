3d47f2ea419800492fdd302a713799cf7acb6434
Masee Hussain <hussainmasee@gmail.com>
2018-09-11 10:51:57 +0100

    adding && isValid on each rule will solve the problem

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -117,15 +117,15 @@ class ContactData extends React.Component {
     let isValid = true;
 
     if (rules.required) {
-      isValid = value.trim() !== "";
+      isValid = value.trim() !== "" && isValid;
     }
 
     if (rules.minLength) {
-      isValid = value.length >= rules.minLength;
+      isValid = value.length >= rules.minLength && isValid;
     }
 
     if (rules.maxLength) {
-      isValid = value.length <= rules.maxLength;
+      isValid = value.length <= rules.maxLength && isValid;
     }
 
     return isValid;
