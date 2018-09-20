c7e5fcd661f376c76d1e452e41b9b53c96e0f210
Masee Hussain <hussainmasee@gmail.com>
2018-09-20 15:05:11 +0100

    imported createStore from redux

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import {createStore} from "redux";
 
 import "./index.css";
 import App from "./App";
 import reducer from "./store/reducer";
+
+const store = 
 
 const app = (
   <Provider>
