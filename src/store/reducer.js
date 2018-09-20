296d467fa4935f979a0fb5453b26202a91e93942
Masee Hussain <hussainmasee@gmail.com>
2018-09-20 15:46:13 +0100

    created switch statement for my two  actionTypes

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -5,6 +5,15 @@ const initialState = {
   totalPrice: 3
 };
 
-const reducer = (state = initialState, action) => {};
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.ADD_INGREDIENT:
+      return {};
+    case actionTypes.REMOVE_INGREDIENT:
+      return {};
+    default:
+      return state;
+  }
+};
 
 export default reducer;
