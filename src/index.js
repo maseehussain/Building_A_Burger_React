4d548635e5b2f8c9a3959a0ada7acd2de2d310db
Masee Hussain <hussainmasee@gmail.com>
2018-09-20 15:05:52 +0100

    created store and injected reducer

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import {createStore} from "redux";
+import { createStore } from "redux";
 
 import "./index.css";
 import App from "./App";
 import reducer from "./store/reducer";
 
-const store = 
+const store = createStore(reducer);
 
 const app = (
   <Provider>
