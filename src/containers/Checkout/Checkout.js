3f040aab326f8a05e1179b14a2f38c9adabf9d6c
Masee Hussain <hussainmasee@gmail.com>
2018-09-21 14:29:33 +0100

    removed componentWIllMount from Checkout.js

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -10,20 +10,6 @@ class Checkout extends React.Component {
     price: 0
   };
 
-  componentWillMount() {
-    const query = new URLSearchParams(this.props.location.search);
-    const ingredients = {};
-    let price = 0;
-    for (let param of query.entries()) {
-      if (param[0] === "price") {
-        price = param[1];
-      } else {
-        ingredients[param[0]] = +param[1];
-      }
-    }
-    this.setState({ ingredients: ingredients, totalPrice: price });
-  }
-
   checkoutCancelledHandler = () => {
     this.props.history.goBack();
   };
