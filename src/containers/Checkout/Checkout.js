27609758c5202ee93cc2944eed776bb7131ab090
Masee Hussain <hussainmasee@gmail.com>
2018-09-21 14:31:19 +0100

    import connect from react-redux into Checkout.js

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route } from "react-router-dom";
+import { connect } from "react-redux";
 
 import CheckoutSummary from "../../components/Order/CheckoutSummary/CheckoutSummary";
 import ContactData from "./ContactData/ContactData";
