f9652d50a1e8a85cf772791ad133b5a94fcf55df
Masee Hussain <hussainmasee@gmail.com>
2018-09-21 13:56:04 +0100

    add total price for remove ingredient

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -26,7 +26,7 @@ const reducer = (state = initialState, action) => {
           ...state.ingredients,
           [action.ingredientName]: state.ingredients[action.ingredientName] + 1
         },
-        totalPrice: state.totalPrice
+        totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
       };
     case actionTypes.REMOVE_INGREDIENT:
       return {
@@ -34,7 +34,8 @@ const reducer = (state = initialState, action) => {
         ingredients: {
           ...state.ingredients,
           [action.ingredientName]: state.ingredients[action.ingredientName] - 1
-        }
+        },
+        totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredientName]
       };
     default:
       return state;
