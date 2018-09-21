81ae0c45076c55240b1cca8abe5374e74a6b1768
Masee Hussain <hussainmasee@gmail.com>
2018-09-21 14:13:45 +0100

    changed updatePurchaseState

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -13,7 +13,6 @@ import * as actionTypes from "../../store/actions";
 
 class BurgerBuilder extends React.Component {
   state = {
-    purchasable: false,
     purchasing: false,
     loading: false,
     error: null
@@ -37,7 +36,7 @@ class BurgerBuilder extends React.Component {
       .reduce((sum, el) => {
         return sum + el;
       }, 0);
-    this.setState({ purchasable: sum > 0 });
+    return sum > 0;
   }
 
   purchaseHandler = () => {
