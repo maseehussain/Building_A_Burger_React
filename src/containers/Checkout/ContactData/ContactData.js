c2358fcf38c7bd67ed3fd64cefd832baf8d4ad18
Masee Hussain <hussainmasee@gmail.com>
2018-09-21 14:39:38 +0100

    import connect into ContactData.js

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { connect } from "react-redux";
 
 import Button from "../../../components/UI/Button/Button";
 import Spinner from "../../../components/UI/Spinner/Spinner";
