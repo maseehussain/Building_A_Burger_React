80a6e0545b18ea72c76ddb1635e84ff687e662c7
Masee Hussain <hussainmasee@gmail.com>
2018-09-24 10:02:06 +0100

    log store.getState

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ const logger = store => {
     return action => {
       console.log("[middleware] dispatching", action);
       const result = next(action);
+      console.log("[Middleware] next state", store.getState());
       return result;
     };
   };
