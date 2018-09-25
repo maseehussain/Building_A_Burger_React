e8fec4b3c51051556bd9d7f422e43666b649b3e7
Masee Hussain <hussainmasee@gmail.com>
2018-09-25 10:26:33 +0100

    Preapared the store folder structure and updated the links

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { createStore } from "redux";
 
 import "./index.css";
 import App from "./App";
-import reducer from "./store/reducer";
+import reducer from "./store/reducers/reducer";
 
 const store = createStore(
   reducer,
