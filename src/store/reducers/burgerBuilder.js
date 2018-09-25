4dcc7ae891a376c372aa5f0bff87eaf2dde1d0ae
Masee Hussain <hussainmasee@gmail.com>
2018-09-25 15:18:32 +0100

    added a case for fetch ingredients failed

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -36,9 +36,14 @@ const reducer = (state = initialState, action) => {
     case actionTypes.SET_INGREDIENTS:
       return {
         ...state,
-        ingredients: action.ingredients
+        ingredients: action.ingredients,
+        error: false
       };
-
+    case actionTypes.FETCH_INGREDIENTS_FAILED:
+      return {
+        ...state,
+        error: true
+      };
     default:
       return state;
   }
