fcefcc992cb3fd3021ea2ddd2fe254600f90da78
Masee Hussain <hussainmasee@gmail.com>
2018-09-27 10:11:03 +0100

    created my action creator for purchase burger success

--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -1,1 +1,9 @@
-import * as actionTypes from "../actions/actionTypes";
+import * as actionTypes from "./actionTypes";
+
+export const purchaseBurgerSuccess = (id, orderData) => {
+  return {
+    type: actionTypes.PURCHASE_BURGER_SUCCESS,
+    orderId: id,
+    orderData: orderData
+  };
+};
