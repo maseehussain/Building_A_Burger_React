47b7a5bf918bdf67b9ef340c6bef00fbba2345bb
Masee Hussain <hussainmasee@gmail.com>
2018-09-27 15:15:05 +0100

    import Redirect from react-router-dom

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 
 import CheckoutSummary from "../../components/Order/CheckoutSummary/CheckoutSummary";
