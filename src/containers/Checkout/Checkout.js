813d580356ac6b12ce1cd63eaf762bf99feca8a3
Masee Hussain <hussainmasee@gmail.com>
2018-09-28 10:10:03 +0100

    changed did mount to will mount

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -7,7 +7,7 @@ import ContactData from "./ContactData/ContactData";
 import * as actions from "../../store/actions/index";
 
 class Checkout extends React.Component {
-  componentDidMount() {
+  componentWillMount() {
     this.props.onInitPurchase();
   }
 
